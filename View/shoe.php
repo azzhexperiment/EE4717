e4c07cd104cbd15d890193decfa86223e1ddd9cc
ZHU Zihao <zihao@zhuzihao.me>
2020-08-26 01:01:56 +0800

    :clown_face: UI mock up

---
+++ View/shoe.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+	<meta name="keywords" content="Oakley">
+	<meta name="description" content="our project">
+	<title>Project</title>
+
+	<!-- Styles -->
+</head>
+
+<body>
+	<!-- Nav bar -->
+	<header>
+		<h1><?php mytitle ?></h1>
+	</header>
+
+	<nav>
+		<b>
+			<a href="index.html">Home</a>
+			<a href="services.html">Serices</a>
+			<a href="contact.html">Contact</a>
+		</b>
+	</nav>
+
+	<!-- Shoe -->
+	shoe img here
+
+	<!-- Product Description -->
+	Shoe info here
+
+	<!-- Footer -->
+	<footer>
+		<small><i>Copyright &copy; 2020 Zhu Zihao</i></small>
+	</footer>
+
+	<!-- Scripts -->
+	<script>
+
+	</script>
+</body>
+
+</html>
