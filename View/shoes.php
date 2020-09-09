cbe867b84bd3d265b83b1194f696d7bbabf25e45
ZHU Zihao <zihao@zhuzihao.me>
2020-09-10 02:39:04 +0800

    :lipstick: add new stylesheet

--- View/shoes.php
+++ View/shoes.php
@@ -5,12 +5,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
-	<meta name="keywords" content="Oakley">
+	<meta name="keywords" content="EE4717 Project">
 	<meta name="description" content="our project">
 	<title>Project</title>
 
 	<!-- Styles -->
+	<link rel="stylesheet" href="/assets/css/grid.css">
 </head>
 
 <body>
