4c4ad434f9229ebc60937a88432a0bf65f72990d
ZHU Zihao <zihao@zhuzihao.me>
2020-10-13 18:37:54 +0800

    New layout contents

---
+++ views/layouts/index/more.php
@@ -0,0 +1,48 @@
+<!-- Main content -->
+<div class="container outline">
+	<div class="row">
+		<div class="col-1">
+			<p>col-1</p>
+		</div>
+		<div class="col-1">
+			<p>col-1</p>
+		</div>
+		<div class="col-1">
+			<p>col-1</p>
+		</div>
+		<div class="col-1">
+			<p>col-1</p>
+		</div>
+		<div class="col-1">
+			<p>col-1</p>
+		</div>
+		<div class="col-1">
+			<p>col-1</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-2">
+			<p>col-2</p>
+		</div>
+		<div class="row">
+			<div class="col-4">
+				hi
+			</div>
+		</div>
+		<div class="col-2">
+			<p>col-2</p>
+		</div>
+		<div class="col-2">
+			<p>col-2</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-3">
+			<p>col-3</p>
+		</div>
+		<div class="col-3">
+			<p>col-3</p>
+		</div>
+	</div>
+</div>
