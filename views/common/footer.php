54a70c57e316362498b729840601b2dbc58ac006
ZHU Zihao <zihao@zhuzihao.me>
2020-10-13 18:00:18 +0800

    :lipstick: New styles

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,8 +1,13 @@
 <!-- Footer -->
-<footer class='container'>
+<footer>
 	<div class="row">
 		<div class="col-12">
 			<small><i>Copyright &copy; 2020 Zhu Zihao</i></small>
 		</div>
 	</div>
 </footer>
+
+</div>
+</body>
+
+</html>
---
+++ views/product.php
@@ -0,0 +1,9 @@
+<?php include('common/header.php') ?>
+
+<!-- Main content -->
+<div class="container">
+	<div class="row">
+	</div>
+</div>
+
+<?php include('common/footer.php') ?>
