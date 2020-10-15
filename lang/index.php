ae6a51e7f2bb236e11073520ca3815aec092b924
ZHU Zihao <zihao@zhuzihao.me>
2020-10-15 22:41:52 +0800

    Templates

---
+++ lang/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// Splash
+
+// For Him
+
+// For Her
+
+// Product MAYBE THIS SHOULD HAVE IT'S OWN PAGE
---
+++ models/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Authenticate user
+ *
+ * @author Zhu Zihao <zhuz0010@e.ntu.edu.sg>
+ * @version 1.0.0
+ */
