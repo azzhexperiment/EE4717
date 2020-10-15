b22b428bb2a98e591fc30ab5eef533299ed6cbac
ZHU Zihao <zihao@zhuzihao.me>
2020-10-15 22:42:21 +0800

    Add semantic structures

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <?php
 
+/**
+ * Scripts
+ */
+
+include('model/auth.php');
+
+/**
+ * Variables
+ */
+
 include('lang/header.php');
+include('lang/index.php');
 include('lang/footer.php');
 
 
-include('layouts/common/header.php');
+/**
+ * Layouts
+ */
 
+include('layouts/common/header.php');
 include('layouts/index/banner.php');
 include('layouts/index/featured.php');
 include('layouts/index/more.php');
-
 include('layouts/common/footer.php');
