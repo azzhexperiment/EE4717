eb50a685a6495f071d77d91242990eef6f2cf0be
ZHU Zihao <zihao@zhuzihao.me>
2020-10-16 13:12:24 +0800

    :lipstick: Remove grid and update footer styles

--- layouts/common/footer.php
+++ layouts/common/footer.php
@@ -1,41 +1,35 @@
 <!-- Footer -->
 <footer class="container">
-	<div class="row text-uppercase">
-		<div class="col-9 links">
-			<div class="row">
-				<div class="col-4">
-					Find out More
-					<ul>
-						<li>who are we?</li>
-						<li>Contact Us</li>
-					</ul>
-				</div>
-				<div class="col-4">
-					<span class="header__footer">
-						join the family
-					</span>
-					<ul>
-						<li><a href="mailto:jobs@cleoandazzh.com">Jobs</a></li>
-					</ul>
-				</div>
-			</div>
+	<div class="links text-uppercase">
+
+		<div class="link-col">
+			<span class="header__footer">Find out More</span>
+			<ul>
+				<li>who are we?</li>
+				<li>Contact Us</li>
+			</ul>
 		</div>
 
-		<div class="col-3 social">
-			<div class="row">
-				<div class="col-4">SOCIAL 1</div>
-				<div class="col-4">SOCIAL 2</div>
-				<div class="col-4">SOCIAL 3</div>
-			</div>
+		<div class="link-col">
+			<span class="header__footer">join the family</span>
+			<ul>
+				<li><a href="mailto:jobs@cleoandazzh.com">Jobs</a></li>
+			</ul>
 		</div>
+	</div>
+
+	<div class="social">
+		<ul>
+			<li>icon 1</li>
+			<li>icon 2</li>
+			<li>icon 3</li>
+		</ul>
 	</div>
 
 	<hr>
 
-	<div class="row">
-		<div class="text-center">
-			<p><?= $copyright ?></p>
-		</div>
+	<div class="text-center">
+		<p><?= $copyright ?></p>
 	</div>
 </footer>
 
