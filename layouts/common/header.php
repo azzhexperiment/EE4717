a72aac094ada1a5e785cbf25081caf10a78bcc5c
ZHU Zihao <zihao@zhuzihao.me>
2020-10-18 00:27:28 +0800

    Extend space for script injection

--- layouts/common/header.php
+++ layouts/common/header.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?= $title ?></title>
-
-	<!-- Styles -->
-	<link rel="stylesheet" href="../../assets/css/style.css">
-
-	<!-- Scripts -->
 </head>
 
 <body>
