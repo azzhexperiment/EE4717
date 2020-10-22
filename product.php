e77f00c1dbd2707ab70ee5bc81972ec277db2e53
ZHU Zihao <zihao@zhuzihao.me>
2020-10-23 00:33:48 +0800

    Activate models

--- product.php
+++ product.php
@@ -8,22 +8,14 @@ include_once('models/Session.php');
 include_once('models/Auth.php');
 include_once('models/connect-db.php');
 include_once('models/Cart.php');
-$imageNum = 4;
+include_once('models/Product.php');
 
 
 //============================================================================//
 // VARIABLES
 //============================================================================//
 
-$productName = 'Product Name';
-
-$title = $productName . 'Shop product - CLEO & AZZH Collection';
-
-$productDescription = 'Bacon ipsum dolor amet capicola doner fatback picanha kielbasa pancetta leberkas burgdoggen pig shankle ball tip kevin landjaeger beef ribs. Filet mignon t-bone short loin pancetta chislic. Fatback venison ham hock pancetta cow shankle tongue prosciutto filet mignon landjaeger pork chop. Ham hock picanha short ribs bresaola tri-tip salami short loin alcatra ground round sirloin chicken pork belly. Buffalo boudin kielbasa kevin meatball strip steak prosciutto hamburger pork chop filet mignon shoulder pork loin.
-
-Tail cupim meatloaf chicken leberkas. Ham tail alcatra fatback cow turkey corned beef. Filet mignon salami pastrami drumstick pancetta, cupim brisket short loin ground round andouille. Bacon drumstick leberkas tri-tip ball tip, chuck shoulder beef ground round short loin pork. Hamburger picanha prosciutto kevin, sausage corned beef jowl beef filet mignon andouille t-bone alcatra flank brisket. Burgdoggen porchetta turkey, leberkas venison ham hock chislic pastrami tail. Spare ribs venison beef ribs, short ribs fatback meatloaf flank shoulder ball tip alcatra landjaeger.';
-
-$productPrice = 10;
+$title = 'Shop product - CLEO & AZZH Collection';
 
 include_once('lang/listings.php');
 
