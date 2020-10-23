07fb233af8a605a4f2f3844270dc0daaf07a9216
ZHU Zihao <zihao@zhuzihao.me>
2020-10-24 03:10:51 +0800

    Update styles

--- layouts/common/header.php
+++ layouts/common/header.php
@@ -1,28 +1,26 @@
 </head>
 
 <body>
-    <!-- Page wrapepr -->
-    <div class="wrapper">
-        <!-- Content container -->
-        <div class="container">
-            <!-- Header -->
-            <header>
-                <div class="header text--center">
-                    <a href="index.php">
-                        <img src="assets/img/common/logo.png" alt="Cleo & AZZH">
-                    </a>
-                </div>
-            </header>
+    <!-- Content container -->
+    <div class="container">
+        <!-- Header -->
+        <header>
+            <div class="header text--center">
+                <a href="index.php">
+                    <img src="assets/img/common/logo.png" alt="Cleo & AZZH">
+                </a>
+            </div>
+        </header>
 
-            <!-- Navigation -->
-            <nav>
-                <div class="nav text--center style--none text--uppercase">
-                    <ul>
-                        <li><a href="listings.php?sex=m">For Him</a></li>
-                        <li><a href="listings.php?sex=f">For Her</a></li>
-                        <li><a href="listings.php?cat=featured">Featured</a></li>
-                        <li><a href="listings.php?cat=new">New Arrival</a></li>
-                        <li><a href="cart.php">Cart</a></li>
-                    </ul>
-                </div>
-            </nav>
+        <!-- Navigation -->
+        <nav>
+            <div class="nav text--center style--none text--uppercase">
+                <ul>
+                    <li><a href="listings.php?sex=m">For Him</a></li>
+                    <li><a href="listings.php?sex=f">For Her</a></li>
+                    <li><a href="listings.php?cat=featured">Featured</a></li>
+                    <li><a href="listings.php?cat=new">New Arrival</a></li>
+                    <li><a href="cart.php">Cart</a></li>
+                </ul>
+            </div>
+        </nav>
