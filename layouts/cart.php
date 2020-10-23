fd390e19e5611f457d8974ff65348a87a8362a7d
ZHU Zihao <zihao@zhuzihao.me>
2020-10-23 18:15:51 +0800

    Isolate cart pages for order and confirmation

--- layouts/cart.php
+++ layouts/cart.php
@@ -2,74 +2,14 @@
 <section class="cart">
     <h1>Your cart</h1>
 
-    <?php if (count($cart->productId)) { ?>
+    <?php
 
-        <!-- TODO: review form classes -->
-        <form action="<?= htmlentities($_SERVER['PHP_SELF']) ?>" method="POST" class="form form__cart" name="confirmOrder" id="form__confirm-order">
+    if (!isset($_GET['saleId'])) {
+        include_once('cart-new.php');
+    } else {
+        include_once('cart-confirm.php');
+    }
 
-            <table class="cart__content">
-
-                <?php for ($i = 0; $i < count($cart->productId); $i++) { ?>
-
-                    <!-- TODO: padding space etc, refer to docs -->
-                    <tr class="cart__item">
-                        <td class="cart__images">
-                            <input type="checkbox" name="productId[<?= $cart->productId[$i] ?>]" checked>
-                        </td>
-                        <td class="cart__images">
-                            <a href="product.php?id=<?= $cart->productId[$i] ?>">
-                                <img src="assets/img/products/<?= $cart->productId[$i] ?>.jpg" class="cart__image">
-                            </a>
-                        </td>
-                        <td class="cart__info">
-                            <a href="product.php?id=<?= $cart->productId[$i] ?>">
-                                <?= $cart->productName[$i] ?>
-                            </a>
-                            <br>
-                            <!-- TODO: make dropdown from list of sizes -->
-                            <input type="checkbox" name="productSize[<?= $cart->productId[$i] ?>]" hidden>
-                            <?= $cart->productSize[$i] ?>
-                            <br>
-                            <input type="checkbox" name="productOrderQty[<?= $cart->productId[$i] ?>]" hidden>
-                            <?= $cart->productOrderQty[$i] ?>
-                        </td>
-                        <td class="cart__subtotal">
-                            <input type="checkbox" name="productSubtotal[<?= $cart->productId[$i] ?>]" hidden>
-                            $<?= $cart->productSubtotal[$i] ?>
-                        </td>
-                    </tr>
-
-                <?php } ?>
-
-                <!-- // TODO: Update on select -->
-                <tr class="cart__total">
-                    <td>
-                        $<?= $cart->productTotal ?>
-                    </td>
-                </tr>
-            </table>
-
-            <!-- EMAIL -->
-            <div class="form__email">
-                Your email:
-                <input type="email" name="customerEmail" class="form__email" id="form__email">
-            </div>
-
-            <!-- TODO: add modal for order confirmation -->
-            <button class="cart--confirm" form="form__confirm-order" value="confirmOrder" type="submit">
-                Confirm Order
-            </button>
-        </form>
-
-    <?php } else { ?>
-        <div class="text-center">
-            <p>
-                Your cart is empty.
-            </p>
-            <p>
-                Go check out our featured listings!
-            </p>
-        </div>
-    <?php } ?>
+    ?>
 
 </section>
