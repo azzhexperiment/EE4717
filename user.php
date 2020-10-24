c8ee58c3c2588e4fd4cd4c2626420450baa7a776
ZHU Zihao <zihao@zhuzihao.me>
2020-10-25 00:55:38 +0800

    Add user files

---
+++ user.php
@@ -0,0 +1,35 @@
+<?php
+
+//============================================================================//
+// MODELS
+//============================================================================//
+
+include_once('models/Session.php');
+include_once('models/Auth.php');
+// include_once('models/connect-db.php');
+// include_once('models/User.php');
+// include_once('models/disconnect-db.php');
+
+
+//============================================================================//
+// VARIABLES
+//============================================================================//
+
+$title = 'Customer - CLEO & AZZH Collection: Neue Urban Fashion';
+
+include_once('lang/user.php');
+
+
+//============================================================================//
+// LAYOUTS
+//============================================================================//
+
+include_once('layouts/common/head.php');
+include_once('layouts/js/user.php');
+include_once('layouts/common/header.php');
+
+echo '<main>';
+include_once('layouts/user.php');
+echo '</main>';
+
+include_once('layouts/common/footer.php');
