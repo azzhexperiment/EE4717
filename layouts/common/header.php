3fbebf595f70b3c0f2f0e2e6733fa8ab28bd63a6
ZHU Zihao <zihao@zhuzihao.me>
2020-10-24 17:52:45 +0800

    Add grid to header

--- layouts/common/header.php
+++ layouts/common/header.php
@@ -3,24 +3,35 @@
 <body>
     <!-- Content container -->
     <div class="container">
-        <!-- Header -->
-        <header>
-            <div class="header text--center">
-                <a href="index.php">
-                    <img src="assets/img/common/logo.png" alt="Cleo & AZZH">
-                </a>
-            </div>
-        </header>
+        <div id="header">
+            <!-- Header -->
+            <header>
+                <div class="header__wrapper">
+                    <div class="placeholder"></div>
+                    <div class="header text--center">
+                        <a href="index.php">
+                            <img src="assets/img/common/logo.png" alt="Cleo & AZZH">
+                        </a>
+                    </div>
+                    <div class="global__member text--right">
+                        <ul>
+                            <li><a href="login.php">Login</a></li>
+                            <!-- // TODO: replace with cart icon -->
+                            <li><a href="cart.php">Cart</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
 
-        <!-- Navigation -->
-        <nav>
-            <div class="nav text--center style--none text--uppercase">
-                <ul>
-                    <li><a href="listings.php?sex=m">For Him</a></li>
-                    <li><a href="listings.php?sex=f">For Her</a></li>
-                    <li><a href="listings.php?cat=featured">Featured</a></li>
-                    <li><a href="listings.php?cat=new">New Arrival</a></li>
-                    <li><a href="cart.php">Cart</a></li>
-                </ul>
-            </div>
-        </nav>
+            <!-- Navigation -->
+            <nav>
+                <div class="nav text--center">
+                    <ul>
+                        <li><a href="listings.php?sex=m">For Him</a></li>
+                        <li><a href="listings.php?sex=f">For Her</a></li>
+                        <li><a href="listings.php?cat=featured">Featured</a></li>
+                        <li><a href="listings.php?cat=new">New Arrival</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
