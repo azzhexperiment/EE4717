0a4503c9b12ef300139b79d1ba16c01806846793
ZHU Zihao <zihao@zhuzihao.me>
2020-10-24 20:22:21 +0800

    Remove unused class

--- layouts/featured.php
+++ layouts/featured.php
@@ -3,17 +3,17 @@
     <h1 class="index__header">Featured</h1>
 
     <div class="featured__grid">
-        <div class="card featured__item">
+        <div class="featured__item">
             <img class="img--fit" src="assets/img/index/featured-1.jpg" alt="">
 
             <button class="shop">Shop</button>
         </div>
-        <div class="card featured__item">
+        <div class="featured__item">
             <img class="img--fit" src="assets/img/index/featured-2.jpg" alt="">
 
             <button class="shop">Shop</button>
         </div>
-        <div class="card featured__item">
+        <div class="featured__item">
             <img class="img--fit" src="assets/img/index/featured-1.jpg" alt="">
 
             <button class="shop">Shop</button>
