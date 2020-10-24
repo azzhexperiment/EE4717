062c3e1466f1e93de614535be105c1696e24506c
ZHU Zihao <zihao@zhuzihao.me>
2020-10-24 19:19:52 +0800

    Update styles

--- index.php
+++ index.php
@@ -25,16 +25,10 @@ include_once('layouts/common/head.php');
 include_once('layouts/js/index.php');
 include_once('layouts/common/header.php');
 
-// START OF MAIN CONTENT
-
 echo '<main>';
-
 include_once('layouts/banner.php');
 include_once('layouts/featured.php');
 include_once('layouts/categories.php');
-
 echo '</main>';
-
-// END OF MAIN CONTENT
 
 include_once('layouts/common/footer.php');
--- layouts/featured.php
+++ layouts/featured.php
@@ -1,16 +1,22 @@
 <!-- Featured items -->
 <section class="featured">
-    <h1>Featured</h1>
+    <h1 class="index__header">Featured</h1>
 
     <div class="featured__grid">
         <div class="card featured__item">
             <img class="img--fit" src="assets/img/index/featured-1.jpg" alt="">
+
+            <button class="shop">Shop</button>
         </div>
         <div class="card featured__item">
             <img class="img--fit" src="assets/img/index/featured-2.jpg" alt="">
+
+            <button class="shop">Shop</button>
         </div>
         <div class="card featured__item">
             <img class="img--fit" src="assets/img/index/featured-1.jpg" alt="">
+
+            <button class="shop">Shop</button>
         </div>
     </div>
 </section>
