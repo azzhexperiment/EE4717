bc52fa32552fd103a55bc86667e71c3696ec3fdd
ZHU Zihao <zihao@zhuzihao.me>
2020-10-25 19:35:06 +0800

    Add comment

--- models/Cart.php
+++ models/Cart.php
@@ -3,35 +3,14 @@
 use Cart\Cart;
 use Sales\Sales;
 
-// echo 'Printing POST from cart.php';
-// echo '<pre>';
-// echo print_r($_POST);
-// echo '</pre>';
-
-// echo 'Printing SESSION from cart.php';
-// echo '<pre>';
-// echo var_dump($_SESSION);
-// echo '</pre>';
-
-echo 1 . '<br>';
-
 // TODO: get cart from DB if logged in, else get from session
 $cart = new Cart();
-
-// echo 'Printing cart from cart.php';
-// echo '<pre>';
-// echo print_r($cart);
-// echo '</pre>';
-
-echo 2 . '<br>';
 
 if (
     !empty($_POST['productId']) &&
     !empty($_POST['customerEmail']) &&
     !empty($cart->productId)
 ) {
-    echo 3 . '<br>';
-
     $saleStatus = 1;
 
     $sales = new Sales(
@@ -46,27 +25,14 @@ if (
         $saleStatus
     );
 
-    echo 4 . '<br>';
-
     $cart = new Cart();
-
-    echo 5 . '<br>';
 }
 
-echo 6 . '<br>';
-
 if (isset($_GET['saleId'])) {
-    echo 7 . '<br>';
     $saleId = (int) $_GET['saleId'];
 
     $sales = new Sales($saleId, 0, 0, 0, 0, 0, 0, 0, 0);
 
+    // TODO: add db
     $cart = $sales->retrieveSaleRecord($saleId);
-
-    // echo 'Printing cart from cart.php';
-    // echo '<pre>';
-    // echo print_r($cart);
-    // echo '</pre>';
 }
-
-echo 8 . '<br>';
