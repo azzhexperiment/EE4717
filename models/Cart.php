ffb2a905c65559b94e1d3819343bad0030ee1ff8
ZHU Zihao <zihao@zhuzihao.me>
2020-11-04 00:51:22 +0800

    Auto refresh after item removal

--- models/Cart.php
+++ models/Cart.php
@@ -9,8 +9,16 @@
 
 use Cart\Cart;
 
+$removeSuccess = '<script>
+    alert("Item removed")
+    window.location = "http://192.168.56.2/f37ee/project/cart.php"
+</script>';
+
 $cart = new Cart();
 
 if (isset($_POST['remove'])) {
     $cart->removeItem($_POST['remove'][0]);
+
+    unset($_POST['remove']);
+    echo $removeSuccess;
 }
