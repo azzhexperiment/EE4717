5f00526900605141d7e23963e351aa300126f898
ZHU Zihao <zihao@zhuzihao.me>
2020-11-03 21:42:21 +0800

    Update index links

--- layouts/featured.php
+++ layouts/featured.php
@@ -4,25 +4,25 @@
 
     <div class="featured__grid">
         <div class="featured__item">
-            <img class="img--fit" src="assets/img/products/12301.jpg" alt="">
+            <a href="product.php?id=12301">
+                <img class="img--fit" src="assets/img/products/12301.jpg" alt="">
 
-            <button class="shop">
-                <a href="product.php?id=12301">Shop</a>
-            </button>
+                <button class="shop">Shop</button>
+            </a>
         </div>
         <div class="featured__item">
-            <img class="img--fit" src="assets/img/products/11301.jpg" alt="">
+            <a href="product.php?id=11301">
+                <img class="img--fit" src="assets/img/products/11301.jpg" alt="">
 
-            <button class="shop">
-                <a href="product.php?id=11301">Shop</a>
-            </button>
+                <button class="shop">Shop</button>
+            </a>
         </div>
         <div class="featured__item">
-            <img class="img--fit" src="assets/img/products/12302.jpg" alt="">
+            <a href="product.php?id=12302">
+                <img class="img--fit" src="assets/img/products/12302.jpg" alt="">
 
-            <button class="shop">
-                <a href="product.php?id=12302">Shop</a>
-            </button>
+                <button class="shop">Shop</button>
+            </a>
         </div>
     </div>
 </section>
